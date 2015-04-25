7bb931378155c22e45403628f6dca0cd3187718e
Macauley <bmacauley@uk155150.local>
2015-04-25 13:01:45 +0100

    first commit

---
+++ vault.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# libs
+import sys
+import click
+
+# global variables
+
+
+# Classes
+
+
+# Functions
+
+
+
+
+
+
+
+
+
+def main():
+    """Main entry point for the script."""
+    
+    # (1) read cli inputs
+
+    # (2) read config file... user, key, server
+
+    # (3) program
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
